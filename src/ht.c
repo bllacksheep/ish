b606237c874a9732d52c922773ab892a85f46b63
bllacksheep <maclighiche@gmail.com>
2026-03-06 17:03:49 +0000

    update: add put var, remove setters too much clutter

--- src/ht.c
+++ src/ht.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define HT_MAX_KEY_LEN 20
+
 typedef struct ht_item {
   char *key;
   char *value;
@@ -11,10 +13,8 @@ typedef struct ht_item {
 typedef ht_item_t ht_table_t[HT_MAX];
 static ht_table_t *ht_table = NULL;
 
-static ht_table_t *ht_init(void);
-static ht_table_t *ht_get_table(void);
-static int ht_set_key(ht_item_t *, char *);
-static int ht_set_val(ht_item_t *, char *);
+static ht_table_t *init(void);
+static ht_table_t *get_table(void);
 static ht_item_t *lookup_item(const ht_table_t *, const char *, const size_t);
 static unsigned get_hash(const char *, const size_t, const unsigned);
 
@@ -23,8 +23,8 @@ static unsigned get_hash(const char *k, const size_t kl, const unsigned at) {
 }
 
 // take table, key and key length and return value
-static ht_item_t *lookup_val(const ht_table_t *tbl, const char *item_key,
-                             const size_t item_key_len) {
+static ht_item_t *lookup_item(const ht_table_t *tbl, const char *item_key,
+                              const size_t item_key_len) {
   unsigned attempt = 0;
   unsigned try = get_hash(item_key, item_key_len, attempt);
 
@@ -45,7 +45,7 @@ static ht_item_t *lookup_val(const ht_table_t *tbl, const char *item_key,
 
 // initialize a table if not already initialized and return pointer to first
 // elem
-static ht_table_t *ht_init(void) {
+static ht_table_t *table_init(void) {
   if (ht_table != NULL) {
     return ht_table;
   }
@@ -57,22 +57,27 @@ static ht_table_t *ht_init(void) {
   return ht_table;
 }
 
-static ht_table_t *ht_get_table(void) {
+static ht_table_t *table_get(void) {
   if (ht_table == NULL) {
-    return ht_init();
+    return table_init();
   }
   return ht_table;
 }
 
 // caller must check is val NULL
 const char *ht_get_var(const char *item_k) {
-  ht_table_t *table = ht_get_table();
+  ht_table_t *table = table_get();
   if (table == NULL) {
-    fprintf(stderr, "i.sh: failed to initialize ht table, code: %d", ERRHTGET);
+    fprintf(stderr, "i.sh: failed to get ht table, code: %d", ERRHTGET);
     exit(ERRHTGET);
   }
-  const size_t max_len = 20;
-  const size_t item_kl = strnlen(item_k, max_len);
+  const size_t item_kl = strnlen(item_k, HT_MAX_KEY_LEN);
+
+  if (item_kl == 0) {
+    fprintf(stderr, "i.sh: failed to initialize key %s, code: %d", item_k,
+            ERRHTINS);
+    return EXIT_FAILURE;
+  }
 
   ht_item_t *item = lookup_item(table, item_k, item_kl);
   if (item != NULL) {
@@ -81,57 +86,42 @@ const char *ht_get_var(const char *item_k) {
   return NULL;
 }
 
-static int ht_set_val(ht_item_t *item, char *v) {
-  if (item == NULL || v == NULL) {
-    fprintf(stderr, "i.sh: failed to set value %s, code: %d", v, ERRHTINS);
-    return EXIT_FAILURE;
+int ht_put_var(const char *item_k, const char *item_v) {
+  ht_table_t *table = table_get();
+  if (table == NULL) {
+    fprintf(stderr, "i.sh: failed to get ht table, code: %d", ERRHTGET);
+    exit(ERRHTGET);
   }
+  const size_t item_kl = strnlen(item_k, HT_MAX_KEY_LEN);
 
-  // if not null, and the same value, overwrite, else .... something else
-  item->value = strdup(v);
-  if (item->value == NULL) {
-    fprintf(stderr, "i.sh: failed to initialize value %s, code: %d", v,
+  if (item_kl == 0) {
+    fprintf(stderr, "i.sh: failed to initialize key %s, code: %d", item_k,
             ERRHTINS);
     return EXIT_FAILURE;
   }
-  return EXIT_SUCCESS;
-}
 
-static int ht_set_key(ht_item_t *item, char *k) {
-  if (item == NULL || k == NULL) {
-    fprintf(stderr, "i.sh: failed to set key %s, code: %d", k, ERRHTINS);
-    return EXIT_FAILURE;
+  ht_item_t *item = lookup_item(table, item_k, item_kl);
+
+  if (item != NULL) {
+    item->key = strdup(item_k);
+    if (item->key == NULL) {
+      fprintf(stderr, "i.sh: failed to initialize key %s, code: %d", item_k,
+              ERRHTINS);
+      return EXIT_FAILURE;
+    }
+
+    item->value = strdup(item_v);
+    if (item->value == NULL) {
+      fprintf(stderr, "i.sh: failed to initialize value %s, code: %d", item_v,
+              ERRHTINS);
+      return EXIT_FAILURE;
+    }
   }
-
-  // if not null, and the same value, overwrite, else .... something else
-  item->key = strdup(k);
-  if (item->key == NULL) {
-    fprintf(stderr, "i.sh: failed to initialize key %s, code: %d", k, ERRHTINS);
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
-}
-
-int ht_put_var(const char *item_k, const char *item_v) {
-  ht_table_t *table = ht_get_table();
-  if (table == NULL) {
-    fprintf(stderr, "i.sh: failed to initialize ht table, code: %d", ERRHTINS);
-    exit(ERRHTINS);
-  }
-
-  if (ht_set_key(ht_item, item_k) != EXIT_SUCCESS) {
-    return EXIT_FAILURE;
-  }
-  if (ht_set_val(ht_item, item_v) != EXIT_SUCCESS) {
-    return EXIT_FAILURE;
-  }
-
   return EXIT_SUCCESS;
 }
 
 int ht_del_var(const char *item_k) {
-  ht_table_t *table = ht_get_table();
+  ht_table_t *table = table_get();
   if (table == NULL) {
     fprintf(stderr, "i.sh: failed to initialize ht table, code: %d", ERRHTDEL);
     exit(ERRHTDEL);
