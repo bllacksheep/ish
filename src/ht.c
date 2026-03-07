5de4c0c413346d56c8d124383be6be052c0ccdf1
bllacksheep <maclighiche@gmail.com>
2026-03-07 12:26:02 +0000

    fix: walk back this test approach

--- src/ht.c
+++ src/ht.c
@@ -1,8 +1,35 @@
-#include "../include/ht.h"
-#include "ht_internal.h"
+#include "ht.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define HT_MAX 1000
+#define HT_MAX_KEY_LEN 20
+
+enum ht_errors {
+  ERRHTINIT = 100,
+  ERRHTGET,
+  ERRHTINS,
+  ERRHTDEL,
+  ERRHTNOKEYLEN,
+  ERRHTNOTABLE,
+  ERRHTNOBUF,
+  ERRHTNOITEM,
+};
+
+typedef struct ht_item {
+  char *key;
+  char *value;
+} ht_item_t;
+
+typedef ht_item_t ht_table_t[HT_MAX];
+static ht_table_t *ht_table = NULL;
+
+static ht_table_t *table_init(void);
+static ht_table_t *table_get(void);
+static ht_item_t *item_lookup(const ht_table_t *, const char *, const size_t);
+static unsigned item_hash(const char *, const size_t, const unsigned);
+static size_t key_get_len(const char *);
 
 static unsigned item_hash(const char *k, const size_t kl, const unsigned at) {
   return 0;
